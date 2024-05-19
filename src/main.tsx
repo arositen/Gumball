86651e84574d4325d4f9dc1a29de9e9db52067e4
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-05-19 16:16:42 -0600

    changed to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ScrollToTop from '@/utils/ScrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
