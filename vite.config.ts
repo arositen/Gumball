d0da2a5aac6179c3adfee9c00d6c38fad05f8a2e
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-05-20 08:21:50 -0600

    changed the resolve alias to include bash path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react()],
   base: '/Gumball/#',
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "/Gumball/#/src") }]
   }
 })
