2a3a29d3a7c2f2255b1aab78e1cb518087f757a2
Jesse Guerrero <jesseguerrero.atx@gmail.com>
2024-05-20 09:57:50 -0600

    back to Browser Router and changed .yml file

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { HashRouter } from "react-router-dom";
+import App from '@/App.tsx'
+import '@/index.css'
+import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from '@/utils/ScrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ScrollToTop />
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gumball/#',
+  base: '/Gumball/',
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "/Gumball/#/src") }]
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
   }
 })
